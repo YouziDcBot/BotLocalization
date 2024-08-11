1b41beb935abf3cfbcbd05545f17154e43b1bddd
柚子 Youzi <yoyowu9601@gmail.com>
2024-08-11 13:06:29 +0800

    修正導出

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { i18next, initI18next, t } = require('./i18n');
 // i18n 示例
-(async () => {
-    await initI18next();
-    console.log(t('package_i18n_done', { lng: 'zh_tw' }));
-})();
+// (async () => {
+//     await initI18next();
+//     console.log(t('package_i18n_done', { lng: 'zh_tw' }));
+// })();
+export { i18next, initI18next, t, translation }
